60a29a9bfd6646e29bdffa1eb9f7485b9bebb22c
opariffazman <ariff.azman@proton.me>
2026-01-05 02:23:00 +0800

    chore: bump version to v0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.t9dialer"
         minSdk = 23
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
     }
 
     buildTypes {
